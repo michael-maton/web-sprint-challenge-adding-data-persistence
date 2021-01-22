8020890c5d12d53ddcbe3b7cf1203699b3987e27
Michael Maton <mjmaton6@gmail.com>
2021-01-21 23:03:48 -0600

    tasks router/model complete and tests working

--- api/task/model.js
+++ api/task/model.js
@@ -7,9 +7,38 @@ module.exports = {
 };
 
 function find() {
-
+  return db("tasks")
+    .join("projects", "tasks.project_id", "projects.project_id")
+    .select(
+      "project_description",
+      "project_name",
+      "task_completed",
+      "task_notes",
+      "task_description"
+    )
+    .then((tasks) => {
+      return tasks.map((task) => {
+        return {
+          ...task,
+          task_completed: task.task_completed === 1 ? true : false,
+        };
+      });
+    });
 }
 
-function addTask(task) {
+async function addTask(task) {
+  const newTaskID = await db("tasks").insert(task);
+  const returnArr = await db("tasks")
+    .where("task_id", newTaskID)
+    .select("task_completed", "task_description", "task_notes")
+    .then((tasks) => {
+      return tasks.map((task) => {
+        return {
+          ...task,
+          task_completed: task.task_completed === 1 ? true : false,
+        };
+      });
+    });
 
+  return returnArr[0];
 }
--- api/task/router.js
+++ api/task/router.js
@@ -8,11 +8,24 @@ const router = express.Router();
 
 
 router.get("/", (req, res) => {
-
+    Tasks.find()
+        .then((tasks) => {
+            res.status(200).json(tasks)
+        })
+        .catch((err) => {
+            res.status(500).json({ err: error.message })
+        })
 })
 
 router.post("/", (req, res) => {
+    const taskData = req.body;
 
+    Tasks.addTask(taskData)
+      .then(task => {
+        res.status(201).json(task);
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'Failed to create new task' });
+      });
 })
-
 module.exports = router;
